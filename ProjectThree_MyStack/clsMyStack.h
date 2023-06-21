1be5ba5fbe84f2616871339eafab2cbf5e87a3c3
mo <muha.11201120@gmail.com>
2023-06-21 22:47:15 +0300

    New methods in class clsMyQueue

--- ProjectThree_MyStack/clsMyStack.h
+++ ProjectThree_MyStack/clsMyStack.h
@@ -1,10 +1,21 @@
 #pragma once
 #include<iostream>
 #include"../projectOne_MyDoublyLinkedList/clsDbLinkedList.h"
+#include"../projectTwo_MyQueu/MyQueue.h"
 template <class T>
-class clsMyStack
+class clsMyStack : public clsMyQueue<T>
 {
-protected:
-	clsDbLinkedList<T> myStack;
 public:
+	void Push( T item )
+	{
+		clsMyQueue<T> _myList.InsertAtBeganing( item );
+	}
+	T Top()
+	{
+		return clsMyQueue<T>::_myList.Front();
+	}
+	T Bottom()
+	{
+		return clsMyQueue<T>::_myList.Back();
+	}
 };
